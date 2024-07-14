09e8bfba9e89b714a5300a4b95a462b8a7c923ac
Chinh <anhvohoanghuy25@gmail.com>
2024-07-14 15:54:42 +0700

    Chinh

---
+++ DAL/ProductDAL.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+
+namespace DAL
+{
+    public class ProductDAL
+    {
+        Pro1QuanLiDienThoaiFinalContext db= new Pro1QuanLiDienThoaiFinalContext();
+        public ProductDAL()
+        {
+            
+        }
+        public List<Product> GetAllProduct()
+        {
+            return db.Products.ToList();
+        }
+
+    }
+}
