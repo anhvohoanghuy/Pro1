ca38981fa881e7abe67b576ccbcdae090f191559
TheAnhLam <anhshy89@gmail.com>
2024-07-17 10:54:46 +0700

    Them Gitignore

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e503f906fb8d274d68100b7391be16efb21d7b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
