60336d806fc0d4a15fc2fafcd2a92a7bbd3407d7
anhvohoanghuy <anhvohoanghuy25@gmail.com>
2024-07-18 08:09:13 +0700

    ignore

--- BUS/obj/Debug/net8.0/BUS.AssemblyInfo.cs
+++ BUS/obj/Debug/net8.0/BUS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BUS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037bf8eaa7bf31e83d982b035d511e0d220ebf06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15fd085d931698edabe0319f7d6446b16719f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("BUS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BUS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
